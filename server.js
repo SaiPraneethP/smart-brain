e6fa70a66267736f8d83ea63d3590356492beebe
SaiPraneethP <75907286+SaiPraneethP@users.noreply.github.com>
2021-10-17 13:29:51 +0530

    chnages to database config

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const knex = require('knex');
 const { json } = require('express');
 
 
-const register =require('./controllers/register')
-const signin=require('./controllers/signin')
-const profile=require('./controllers/profile')
-const image=require('./controllers/image')
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
 
 
 const db = knex({
@@ -17,26 +17,27 @@ const db = knex({
     connection: {
         connectionString: process.env.DATABASE_URL,
         ssl: {
-        rejectUnauthorized: false,
-        },
-        },
+            rejectUnauthorized: false
+        }
+    }
 });
+
 
 
 app.use(cors())
 app.use(express.json());
 
-app.get("/", (req, res) => {res.send("it's working!")})
+app.get("/", (req, res) => { res.send("it's working!") })
 
-app.post('/signin', (req,res)=>{signin.handleSignin(req,res,db,bcrypt)})
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 
-app.post('/register',(req,res)=>{ register.handleRegister(req,res,db,bcrypt)})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
-app.get('/profile/:id', (req,res)=>{profile.handleProfile(req,res,db)} )
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
 
-app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
-app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 
 app.listen(process.env.PORT || 3004, () => {
