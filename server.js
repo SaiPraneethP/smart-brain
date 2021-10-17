dc6a534ba4fa82d3c6e33ee354604b59201cf235
SaiPraneethP <75907286+SaiPraneethP@users.noreply.github.com>
2021-10-17 13:16:53 +0530

    adding database config

--- server.js
+++ server.js
@@ -15,11 +15,8 @@ const image=require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'postgresql-metric-94349',
-        port: 5432,
-        user: 'postgres',
-        password: 'praneeth237',
-        database: 'smart-brain'
+        connectionString: process.env.DATABASE_URL,
+        ssl:true
     }
 });
 
